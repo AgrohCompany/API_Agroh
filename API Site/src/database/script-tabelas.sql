84a8575fc93d0e44677ecb45369b518bd4921af2
AndreyRl <andrey.ordelino@sptech.school>
2022-05-28 20:24:20 -0300

    Atualização do script banco de dados

--- API Site/src/database/script-tabelas.sql
+++ API Site/src/database/script-tabelas.sql
@@ -67,29 +67,70 @@ momento datetime default current_timestamp,
 foreign key (fkSensor) references sensor(idSensor)
 );
 
+create table dados_sensor (
+idDado int primary key auto_increment,
+fkSensor int,
+temperatura double,
+umidade double,
+momento datetime default current_timestamp,
+foreign key (fkSensor) references sensor(idSensor)
+);
 
 /* para sql server - remoto - produção */
 
--- CREATE TABLE usuario (
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	nome VARCHAR(50),
--- 	email VARCHAR(50),
--- 	senha VARCHAR(50),
--- );
+create table Cliente(
+idCliente int primary key IDENTITY(1,1),
+nome varchar(100),
+email varchar(100),
+senha varchar(45),
+cpf_cnpj varchar(45),
+endereco varchar(100),
+cep varchar(12),
+complemento varchar(100),
+estado char(2)
+);
 
--- CREATE TABLE aviso (
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	titulo VARCHAR(100),
---     descricao VARCHAR(150),
--- 	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
--- ); 
+create table tipo_usuario (
+idTipo int primary key IDENTITY(1,1),
+tipoUsuario varchar(45)
+);
 
--- CREATE TABLE medida (
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	temperatura DECIMAL,
--- 	umidade DECIMAL,
--- 	momento DATETIME,
--- 	fk_aquario INT
--- );
+create table usuario (
+idUsuario int primary key IDENTITY(1,1),
+nome varchar(100),
+email varchar(100),
+senha varchar(100),
+cpf varchar (15),
+telefone varchar(45),
+fkCliente int foreign key references cliente(idCliente),
+fkTipo int foreign key references tipo_usuario(idTipo)
+);
+
+create table fazenda (
+idFazenda int primary key IDENTITY(1,1),
+hectar decimal,
+tipoPlantacao varchar(15),
+endereco varchar(150),
+complemento varchar(150),
+cep varchar(12),
+cidade varchar(100),
+estado char(2),
+fkCliente int foreign key references cliente(idCliente)
+);
+
+create table sensor (
+idSensor int primary key IDENTITY (1,1),
+nomeSensor varchar(50),
+regiaoFazenda varchar(20),
+fkFazenda int foreign key references fazenda(idFazenda)
+);
+
+create table dados_sensor (
+idDado int primary key IDENTITY(1,1),
+temperatura decimal,
+umidade decimal,
+momento datetime default current_timestamp,
+fkSensor int foreign key references sensor(idSensor)
+);
 
 
