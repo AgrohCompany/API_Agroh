5c248f1a781ea9ed5819fcab65822d35f13c00f9
Matheus Nascimento <matheus.noliveira@sptech.school>
2022-05-29 15:28:28 -0300

    Banco em Produção

--- API Site/src/models/medidaModel.js
+++ API Site/src/models/medidaModel.js
@@ -1,25 +1,25 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idDado, limite_linhas) {
-    instrucaoSql = `select 
+    instrucaoSql = `select top ${limite_linhas}
                         temperatura, 
                         umidade,
                         FORMAT(momento,'hh:mm:ss') as momento_grafico
                     from dados_sensor
                     where fkSensor = ${idDado}
-                    order by idDado desc limit ${limite_linhas}`;
+                    order by idDado desc`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idDado) {
-    instrucaoSql = `select 
+    instrucaoSql = `select top 1
                         temperatura, 
                         umidade, 
                         FORMAT(momento,'hh:mm:ss') as momento_grafico, 
                         fkSensor 
                         from dados_sensor where fkSensor = ${idDado} 
-                    order by idDado desc limit 1`;
+                    order by idDado desc`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
